3321b6e9ebe3eb611f26359e31e0f18c66164636
Sakar Chauhan <saakarchauhan@gmail.com>
2022-10-15 16:43:35 +0530

    add coffee loader.

--- src/components/common/Loader/index.js
+++ src/components/common/Loader/index.js
@@ -3,7 +3,10 @@ import style from '../../../../styles/Loader.module.css'
 const Loader = () => {
   return (
     <div className='flex justify-center items-center w-full h-screen fixed top-0 left-0 bg-black'>
-        <div className={`${style["bar-spin"]}`}>
+        <div className={`${style["coffee"]}`}>
+          <div className="child-1"></div>
+          <div className="child-2"></div>
+          <div className="child-3"></div>
         </div>
     </div>
   )
