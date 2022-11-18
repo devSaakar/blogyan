34ef7fd193c7907b15772323200dc6632d24c5b7
Sakar Chauhan <saakarchauhan@gmail.com>
2022-11-18 19:49:22 +0530

    feat : add welcome for new user.

--- src/components/common/Auth/Login.jsx
+++ src/components/common/Auth/Login.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import Image from 'next/image'
-import { auth, provider } from '../../../../config/firebase';
-import { signInWithPopup } from 'firebase/auth';
+import React from "react";
+import Image from "next/image";
+import { auth, provider } from "../../../../config/firebase";
+import { signInWithPopup } from "firebase/auth";
 
-import GoogleLogo from '../../../assets/google.png';
-
+import GoogleLogo from "../../../assets/google.png";
 
 const Login = () => {
-  const loginWithGoogle = () =>{
-    signInWithPopup(auth,provider);
-  }
-  
-  return (
-    <div className='container mx-auto flex h-screen justify-center items-center bg-slate-800'>
-        <button onClick={loginWithGoogle} className='bg-white drop-shadow-2xl flex py-2 px-3 rounded-md gap-x-2 text-black font-semibold'><Image src={GoogleLogo} width={25} height={25} alt='google.png'/>Login with Google</button>
-    </div>
-  )
-}
+  const loginWithGoogle = () => {
+      signInWithPopup(auth, provider);
+  };
 
-export default Login
+  return (
+    <div className="container mx-auto flex flex-col h-screen justify-center items-center bg-slate-800 gap-16">
+      <p className="text-primary text-3xl font-serif">Welcome to <span className="text-secondary"> Bro Code</span></p>
+      <button
+        onClick={loginWithGoogle}
+        className="bg-white drop-shadow-2xl flex py-2 px-3 rounded-md gap-x-2 text-black font-semibold"
+      >
+        <Image src={GoogleLogo} width={25} height={25} alt="google.png" />
+        Login with Google
+      </button>
+      <p className="text-secondary text-sm font-serif">
+        Please <span className="text-primary cursor-pointer"> Login</span> To Continue
+      </p>
+    </div>
+  );
+};
+
+export default Login;
